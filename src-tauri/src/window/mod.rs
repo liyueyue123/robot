406484be45a6442c71d9bf267980806ec370b79f
mickytx <363755755@qq.com>
2023-09-25 17:08:18 +0800

    🐞 fix: 隐藏标题栏

--- src-tauri/src/window/mod.rs
+++ src-tauri/src/window/mod.rs
@@ -1,6 +1,5 @@
 use crate::APP;
 
-#[cfg(not(target_os = "linux"))]
 pub fn create_external_windows(label: &str, external_url: &str) {
     let app = APP.get().unwrap();
     tauri::WindowBuilder::new(
@@ -9,20 +8,8 @@ pub fn create_external_windows(label: &str, external_url: &str) {
         tauri::WindowUrl::External(external_url.parse().unwrap()),
     )
     .min_inner_size(1000.00, 600.00)
-    .hidden_title(true)
-    .decorations(true)
-    .build()
-    .expect("failed to build window");
-}
-#[cfg(target_os = "linux")]
-pub fn create_external_windows(label: &str, external_url: &str) {
-    let app = APP.get().unwrap();
-    tauri::WindowBuilder::new(
-        app,
-        label, /* the unique window label */
-        tauri::WindowUrl::External(external_url.parse().unwrap()),
-    )
-    .min_inner_size(1000.00, 600.00)
+    .title("")
+    // .hidden_title(true)
     .decorations(true)
     .build()
     .expect("failed to build window");
