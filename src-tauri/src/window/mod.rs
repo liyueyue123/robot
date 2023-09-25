b1450b4cbbf6a9525cab88a544f4aca9fa0342d8
mickytx <363755755@qq.com>
2023-09-25 16:59:12 +0800

    🐞 fix: create external windows

--- src-tauri/src/window/mod.rs
+++ src-tauri/src/window/mod.rs
@@ -1,5 +1,6 @@
 use crate::APP;
 
+#[cfg(not(target_os = "linux"))]
 pub fn create_external_windows(label: &str, external_url: &str) {
     let app = APP.get().unwrap();
     tauri::WindowBuilder::new(
@@ -9,6 +10,19 @@ pub fn create_external_windows(label: &str, external_url: &str) {
     )
     .min_inner_size(1000.00, 600.00)
     .hidden_title(true)
+    .decorations(true)
+    .build()
+    .expect("failed to build window");
+}
+#[cfg(target_os = "linux")]
+pub fn create_external_windows(label: &str, external_url: &str) {
+    let app = APP.get().unwrap();
+    tauri::WindowBuilder::new(
+        app,
+        label, /* the unique window label */
+        tauri::WindowUrl::External(external_url.parse().unwrap()),
+    )
+    .min_inner_size(1000.00, 600.00)
     .decorations(true)
     .build()
     .expect("failed to build window");
