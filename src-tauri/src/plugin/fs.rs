4b02e0c3eab41b439da8b3a56ada15f9a769f9d2
mickytx <363755755@qq.com>
2023-09-27 12:51:22 +0800

    🐞 fix: 修改配置文件目录

--- src-tauri/src/plugin/fs.rs
+++ src-tauri/src/plugin/fs.rs
@@ -6,7 +6,7 @@ use tauri::{api::path, command};
 #[command]
 pub async fn read_extra_url() -> String {
     let path: PathBuf = path::home_dir().expect("文件打开失败");
-    let file_path = path.as_path().join("robot_conf/router.json");
+    let file_path = path.as_path().join("robot_config/router.json");
     info!("router.json path:{}", file_path.display());
     match std::fs::read_to_string(file_path) {
         Ok(contents) => contents,
